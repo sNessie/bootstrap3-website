60cf1efde3b62f4d35445d108b23346b9ea96187
sNessie <a.czajczyk@icloud.com>
2018-10-07 13:13:33 +0200

    js scroll to section

--- js/script.js
+++ js/script.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-
+    
     $('.navbar-collapse a').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
-
     $('html').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
@@ -18,5 +17,25 @@ $(document).ready(function() {
       }
   });
 
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
 
-})
+    $('a').each(function () {
+        $(this).removeClass('my_link-active');
+    })
+    $(this).addClass('my_link-active');
+    $("#id").animate({"scrollTop": $("#id").scrollTop() + 100});
+    var target = this.hash,
+        menu = target,
+        headerHeight = $(".my_navbar").height(),
+        scrollToPosition = $(target).offset().top - headerHeight;
+    $target = $(target);
+    $('html').animate({ 'scrollTop': scrollToPosition }, 600, function(target){
+        window.location.hash = target;
+    });
+});
+
+
+
+});

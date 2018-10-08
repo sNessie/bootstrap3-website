1a6fd27b6592efe1b1b3fdbf51e88104efd78b83
sNessie <a.czajczyk@icloud.com>
2018-10-08 09:30:58 +0200

    gallery modal window

--- js/script.js
+++ js/script.js
@@ -29,6 +29,14 @@ $(document).ready(function() {
         });
     });
 
+    $('.thumbnail').click(function(){
+        $('.modal-body').empty();
+        var title = $(this).parent('a').attr("title");
+        $('.modal-title').html(title);
+        $($(this).parents('div').html()).appendTo('.modal-body');
+        $('#myModal').modal({show:true});
+    });
+
 
     function onScroll(event){
         var scrollPos = $(window).scrollTop(),

09922e8787e8666521925715344fa0afa719c1e0
sNessie <a.czajczyk@icloud.com>
2018-10-09 11:14:22 +0200

    js mobile hide menuafter click

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 
+    $("body").click(function() {
+         if ($(".navbar-collapse").is(":visible") && $(".navbar-toggle").is(":visible") ) {
+            $('.navbar-collapse').collapse('toggle');
+        }
+  });
+    
     $(window).on("scroll", onScroll);
 
     $(window).on("scroll", function() {
